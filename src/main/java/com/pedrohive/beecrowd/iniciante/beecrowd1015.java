95aacbd2db58d666c318353c8c7f7445013778a0
João Pedro dos Santos Souza <pedrohive7@gmail.com>
2026-04-24 02:07:31 -0300

    chore: Exercício 1015

---
+++ src/main/java/com/pedrohive/beecrowd/iniciante/beecrowd1015.java
@@ -0,0 +1,23 @@
+package com.pedrohive.beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class beecrowd1015 {
+    
+    public static void main (String[] args){
+        
+        Scanner sc = new Scanner (System.in);
+        
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        
+        double resultado = Math.sqrt (Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+        
+         System.out.printf("%.4f\n", resultado);
+        
+        
+    }
+    
+}
